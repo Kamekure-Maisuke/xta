ac48b702d3ff8779f32d997799fc6c84c89af8ed
kamekuramaisuke <taisukeoono350@gmail.com>
2023-06-01 22:04:17 +0900

    update users table

--- initdb/2_init_table.sql
+++ initdb/2_init_table.sql
@@ -8,8 +8,9 @@ CREATE TABLE todos (
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  level INT NOT NULL
 );
 
 INSERT INTO todos (title) VALUES ('タスク1');
