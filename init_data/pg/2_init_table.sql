b2ddbcf2812b08d3ed350d862526247bc6bc71bb
kamekuramaisuke <taisukeoono350@gmail.com>
2023-09-09 08:57:15 +0900

    sql修正

--- init_data/pg/2_init_table.sql
+++ init_data/pg/2_init_table.sql
@@ -1,17 +1,30 @@
 \c sample;
 
-CREATE TABLE todos (
-  id SERIAL PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  completed BOOLEAN NOT NULL DEFAULT false
-);
-
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   level INT NOT NULL
 );
+
+CREATE TABLE todos (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  completed BOOLEAN NOT NULL DEFAULT false,
+  user_id INT REFERENCES users(id),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE todo_history (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id),
+  todo_id INT REFERENCES todos(id),
+  action_type CHAR(1) NOT NULL DEFAULT '0',
+  action_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 
 INSERT INTO todos
   (title)
